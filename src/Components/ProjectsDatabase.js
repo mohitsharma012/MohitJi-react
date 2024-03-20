7cf8490c6436fe7b02b67065cbf28a3d52e84c14
mohitsharma012 <stayer.mohit@gmail.com>
2024-03-20 23:10:35 +0530

    added react files 2

---
+++ src/Components/ProjectsDatabase.js
@@ -0,0 +1,77 @@
+const ProjectsDatabase = [
+    {
+        ProjectId : 1,
+        ProjectName : 'AlleyBot - An AI Friend to Talk With',
+        ProjectDescription: 'AlleyBot is a cutting-edge chatbot application developed using the Django framework. It integrates advanced AI algorithms to simulate engaging and realistic conversations, providing users with an interactive and user-friendly chatting experience. This project showcases my skills in Python, Django, and AI, demonstrating my ability to create intuitive and intelligent web applications.',
+        ProjectSkills : 'HTML5 · Python (Programming Language) · dajngo · Cascading Style Sheets (CSS) · JavaScript · SQLite · OpenAI Products',
+        GitLink : 'https://github.com/mohitsharma012/alleybot.git',
+        LiveLink : 'https://alleybot.onrender.com/',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img2 : '/ProjectImages/project1_2.jpeg',
+        img3 : '/ProjectImages/project1_3.jpeg',
+        
+    },
+    {
+        ProjectId : 2,
+        ProjectName : '𝐕𝐨𝐭𝐞𝐗 - 𝐁𝐥𝐨𝐜𝐤𝐜𝐡𝐚𝐢𝐧-𝐁𝐚𝐬𝐞𝐝 𝐎𝐧𝐥𝐢𝐧𝐞 𝐕𝐨𝐭𝐢𝐧𝐠 𝐒𝐲𝐬𝐭𝐞𝐦',
+        ProjectDescription: 'VoteX represents a secure and innovative online voting platform leveraging blockchain technology. This system ensures tamper-proof records, user authentication, and stringent security measures, guaranteeing a transparent and confidential voting process.',
+        ProjectSkills : ' Ethereum · HTML5 · Python (Programming Language) · Django · JavaScript · Artificial Intelligence (AI) · Relational Databases',
+        GitLink : '',
+        LiveLink : '',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        
+    },
+    {
+        ProjectId : 2,
+        ProjectName : '',
+        ProjectDescription: '',
+        ProjectSkills : '',
+        GitLink : '',
+        LiveLink : '',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        
+    },
+    {
+        ProjectId : 2,
+        ProjectName : '',
+        ProjectDescription: '',
+        ProjectSkills : '',
+        GitLink : '',
+        LiveLink : '',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        
+    },
+    {
+        ProjectId : 2,
+        ProjectName : '',
+        ProjectDescription: '',
+        ProjectSkills : '',
+        GitLink : '',
+        LiveLink : '',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        
+    },
+    {
+        ProjectId : 2,
+        ProjectName : '',
+        ProjectDescription: '',
+        ProjectSkills : '',
+        GitLink : '',
+        LiveLink : '',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        img1 : '/ProjectImages/project1_1.jpeg',
+        
+    },
+   
+ 
+]
+export default ProjectsDatabase
